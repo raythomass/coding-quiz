41fb413399007b87ef73d841fe2774aec274e206
Ray Thomas <rthomas5678@gmail.com>
2023-09-16 23:02:37 -0700

    added more questions

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -22,11 +22,20 @@ var questionOne = {
     answers: ['Strings', 'Booleans', 'Alerts', 'Numbers'],
     correctAnswer : 'Alerts'
 };
+var questionTwo = {
+    title: "The condition in an if/else statement is enclosed with __________",
+    answers: ['Quotes', 'Curly Brackets', 'Parenthesis', 'Square Brackets'],
+    correctAnswer : 'Curly Brackets'
+};
+var questionThree = {
+    title: "Arryays in JavaScript can be used to store __________",
+    answers: ['Numbers and Strings', 'Other Arrys', 'Booleans', 'All of the above'],
+    correctAnswer : 'All of the above'
+};
 
 function subtractTime(){
     seconds--;
 }
-
 function startQuiz(){
     quizTitle.textContent = "Coding Quiz";
     quizDescr.textContent = "Test your coding knowledge with this timed quiz";
@@ -36,7 +45,6 @@ function startQuiz(){
     // answerThree.remove();
     // answerFour.remove();
 }
-
 function setTimer(){
     var timeInterval = setInterval(function(){
         seconds--;
@@ -83,43 +91,182 @@ function askQuestionOne(){
     btnElOne.addEventListener('click', function(){
         if(btnElOne.textContent == questionOne.correctAnswer){
             console.log("Correct");
+            askQuestionTwo();
         }
         else if(btnElOne.textContent !== questionOne.correctAnswer){
             console.log("Incorrect");
             subtractTime();
+            askQuestionTwo();
         };
     });
 
     btnElTwo.addEventListener('click', function(){
         if(btnElTwo.textContent == questionOne.correctAnswer){
             console.log("Correct");
+            askQuestionTwo();
         }
         else if(btnElTwo.textContent !== questionOne.correctAnswer){
             console.log("Incorrect");
             subtractTime();
+            askQuestionTwo();
         };
     });
 
     btnElThree.addEventListener('click', function(){
         if(btnElThree.textContent == questionOne.correctAnswer){
             console.log("Correct");
+            askQuestionTwo();
         }
         else if(btnElThree.textContent !== questionOne.correctAnswer){
             console.log("Incorrect");
             subtractTime();
+            askQuestionTwo();
         };
     });
 
     btnElFour.addEventListener('click', function(){
         if(btnElFour.textContent == questionOne.correctAnswer){
             console.log("Correct");
+            askQuestionTwo()
         }
         else if(btnElFour.textContent !== questionOne.correctAnswer){
+            console.log("Incorrect");
+            subtractTime();
+            askQuestionTwo();
+
+        };
+    });
+}
+
+function askQuestionTwo(){
+    questionTitle.textContent = questionTwo.title;
+
+    btnElOne.classList.add('answer');
+    btnElOne.textContent = questionTwo.answers[0];
+    questionSection.appendChild(btnElOne);
+
+    btnElTwo.classList.add('answer');
+    btnElTwo.textContent = questionTwo.answers[1];
+    questionSection.appendChild(btnElTwo);
+
+    btnElThree.classList.add('answer');
+    btnElThree.textContent = questionTwo.answers[2];
+    questionSection.appendChild(btnElThree);
+
+    btnElFour.classList.add('answer');
+    btnElFour.textContent = questionTwo.answers[3];
+    questionSection.appendChild(btnElFour);
+
+    btnElOne.addEventListener('click', function(){
+        if(btnElOne.textContent == questionTwo.correctAnswer){
+            console.log("Correct");
+            askQuestionThree();
+        }
+        else if(btnElOne.textContent !== questionTwo.correctAnswer){
+            console.log("Incorrect");
+            subtractTime();
+            askQuestionThree();
+        };
+    });
+
+    btnElTwo.addEventListener('click', function(){
+        if(btnElTwo.textContent == questionTwo.correctAnswer){
+            console.log("Correct");
+            askQuestionThree();
+        }
+        else if(btnElTwo.textContent !== questionTwo.correctAnswer){
+            console.log("Incorrect");
+            subtractTime();
+            askQuestionThree();
+        };
+    });
+
+    btnElThree.addEventListener('click', function(){
+        if(btnElThree.textContent == questionTwo.correctAnswer){
+            console.log("Correct");
+            askQuestionThree();
+        }
+        else if(btnElThree.textContent !== questionTwo.correctAnswer){
+            console.log("Incorrect");
+            subtractTime();
+            askQuestionThree();
+        };
+    });
+
+    btnElFour.addEventListener('click', function(){
+        if(btnElFour.textContent == questionTwo.correctAnswer){
+            console.log("Correct");
+            askQuestionThree();
+        }
+        else if(btnElFour.textContent !== questionTwo.correctAnswer){
+            console.log("Incorrect");
+            subtractTime();
+            askQuestionThree();
+        };
+    });
+}
+
+function askQuestionThree(){
+    questionTitle.textContent = questionThree.title;
+
+    btnElOne.classList.add('answer');
+    btnElOne.textContent = questionThree.answers[0];
+    questionSection.appendChild(btnElOne);
+
+    btnElTwo.classList.add('answer');
+    btnElTwo.textContent = questionThree.answers[1];
+    questionSection.appendChild(btnElTwo);
+
+    btnElThree.classList.add('answer');
+    btnElThree.textContent = questionThree.answers[2];
+    questionSection.appendChild(btnElThree);
+
+    btnElFour.classList.add('answer');
+    btnElFour.textContent = questionThree.answers[3];
+    questionSection.appendChild(btnElFour);
+
+    btnElOne.addEventListener('click', function(){
+        if(btnElOne.textContent == questionThree.correctAnswer){
+            console.log("Correct");
+        }
+        else if(btnElOne.textContent !== questionThree.correctAnswer){
+            console.log("Incorrect");
+            subtractTime();
+        };
+    });
+
+    btnElTwo.addEventListener('click', function(){
+        if(btnElTwo.textContent == questionThree.correctAnswer){
+            console.log("Correct");
+        }
+        else if(btnElTwo.textContent !== questionThree.correctAnswer){
+            console.log("Incorrect");
+            subtractTime();
+        };
+    });
+
+    btnElThree.addEventListener('click', function(){
+        if(btnElThree.textContent == questionThree.correctAnswer){
+            console.log("Correct");
+        }
+        else if(btnElThree.textContent !== questionThree.correctAnswer){
+            console.log("Incorrect");
+            subtractTime();
+        };
+    });
+
+    btnElFour.addEventListener('click', function(){
+        if(btnElFour.textContent == questionThree.correctAnswer){
+            console.log("Correct");
+        }
+        else if(btnElFour.textContent !== questionThree.correctAnswer){
             console.log("Incorrect");
             subtractTime();
         };
     });
 }
+
+
 
 
 
